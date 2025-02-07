c6800dfdd3c6125a9dea8b0377e614f2c3f7b540
NateDCoder <rubyandtwixie@gmail.com>
2025-02-07 12:37:40 -0500

    it worked

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,7 +11,6 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
 
-// hi
 package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
